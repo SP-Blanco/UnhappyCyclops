c642fce3723710a5a3b3ab85b9331c473adf54e1
Sean Stewart Stevens <seanstewartstevens@Seans-MacBook-Pro.local>
2019-05-01 08:40:04 +0200

    committing for ben

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mainContainer">
+        <header>
+          <div>The Unhappy Cyclops</div>
+          <button>Get to know us</button>
+          <button>Episodes</button>
+          <button>Hit us up</button>
+        </header>
+        <img className="stewFace" src="./graffiti-wallpaper-12.jpg" />
+      </div>
     </div>
   );
 }
